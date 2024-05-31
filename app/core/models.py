51398e9c74c4a543c15fb672929ea4d7fceda9bf
Madhav <sharmamadhav208@gmail.com>
2024-06-01 01:01:21 +0530

    added student model and studentmanager

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
+from django.contrib.auth.models import(
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 # Create your models here.
+
+class StudentManager(BaseUserManager):
+
+    def create_user(self,enrollment,password=None,**extra_field):
+        user=self.model(enrollment=enrollment,**extra_field)
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+    
+    def create_superuser(self,enrollment,password):
+        user=self.create_user(enrollment,password)
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+class Student(AbstractBaseUser,PermissionsMixin):
+
+    enrollment=models.CharField(max_length=100,unique=True)
+    email=models.EmailField(max_length=255,unique=True)
+    name=models.CharField(max_length=255)
+    classid=models.CharField(max_length=100)
+    degreeid=models.CharField(max_length=100)
+    enrollmentYear=models.IntegerField()
+    is_active=models.BooleanField(default=True)
+
+    objects=StudentManager()
+
+    USERNAME_FIELD= 'enrollment'
+
