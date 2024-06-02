a347cfac650ee80ba1ee51d89794980b40370071
Madhav <sharmamadhav208@gmail.com>
2024-06-02 23:42:58 +0530

    customized admin and student model

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,54 @@
+"""
+Django admin customization.
+"""
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['enrollment', 'name']
+    # This tuple defines the layout and grouping of fields in the user detail view (when editing or creating a user). Each tuple inside fieldsets represents 
+    # a section in the form, with the first element being the section's label (or None for no label), and the second element being a dictionary containing the fields for that section.
+    fieldsets = (
+        (None, {'fields': ('enrollment', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        ( _('Permissions'),
+                {
+                    'fields': (
+                        'is_active',
+                        'is_staff',
+                        'is_superuser',
+                    )
+                }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+
+    """
+        The first section has no label and contains the email and password fields.
+        The second section is labeled "Personal Info" and contains the name field.
+        The third section is labeled "Permissions" and contains the is_active, is_staff, and is_superuser fields.
+    """
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'enrollment',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
+
+
+admin.site.register(models.Student, UserAdmin)
--- app/core/models.py
+++ app/core/models.py
@@ -18,6 +18,7 @@ class StudentManager(BaseUserManager):
     
     def create_superuser(self,enrollment,password):
         user=self.create_user(enrollment,password)
+        user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
 
@@ -26,13 +27,14 @@ class StudentManager(BaseUserManager):
 class Student(AbstractBaseUser,PermissionsMixin):
 
     enrollment=models.CharField(max_length=100,unique=True)
-    email=models.EmailField(max_length=255,unique=True)
+    #email=models.EmailField(max_length=255,unique=True)
     name=models.CharField(max_length=255)
-    classid=models.CharField(max_length=100)
-    degreeid=models.CharField(max_length=100)
-    enrollmentYear=models.IntegerField()
+    #classid=models.CharField(max_length=100)
+    #degreeid=models.CharField(max_length=100)
+    #enrollmentYear=models.IntegerField()
     is_active=models.BooleanField(default=True)
-
+    is_staff = models.BooleanField(default=False)
+    
     objects=StudentManager()
 
     USERNAME_FIELD= 'enrollment'
