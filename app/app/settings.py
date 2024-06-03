5f998b9ab5ebb4f4927ad16074cbd2ee74700dc9
Madhav <sharmamadhav208@gmail.com>
2024-06-03 23:06:44 +0530

    user app updated serializer and urls

--- app/app/settings.py
+++ app/app/settings.py
@@ -44,9 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'user',
     'rest_framework',
     'drf_spectacular',
-    'user',
+    'rest_framework.authtoken',
+    
 ]
 
 MIDDLEWARE = [
